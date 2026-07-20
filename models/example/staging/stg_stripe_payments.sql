560ceddab2cf1b5498a8e6db0c585975c1bfb6db
Chirag Arora <arora2002chirag@gmail.com>
2026-07-20 18:45:44 +0000

    Added sources yaml file, payments , fact_orders files

---
+++ models/example/staging/stg_stripe_payments.sql
@@ -0,0 +1,23 @@
+with 
+
+source as (
+
+    select * from {{ source('stripe', 'payments') }}
+
+),
+
+renamed as (
+
+    select
+        ID as payment_id,
+        ORDERID as order_id,
+        PAYMENTMETHOD as payment_method,
+        STATUS as payment_status,
+        AMOUNT as payment_amount,
+        CREATED as payment_created
+
+    from source
+
+)
+
+select * from renamed
